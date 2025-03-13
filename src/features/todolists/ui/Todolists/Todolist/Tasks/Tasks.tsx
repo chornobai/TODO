c188cfac3c87b4ef887515d49fe7655b98b41b02
veronikachernobai <bombeze13@gmail.com>
2025-03-14 00:40:50 +0300

    add

---
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -0,0 +1,42 @@
+import List from "@mui/material/List"
+import { TaskStatus } from "common/enums"
+import { useGetTasksQuery } from "../../../../api/tasksApi"
+import { DomainTodolist } from "../../../../lib/types/types"
+import { TasksSkeleton } from "../../../skeletons/TasksSkeleton/TasksSkeleton"
+import { Task } from "./Task/Task"
+
+type Props = {
+  todolist: DomainTodolist
+}
+
+export const Tasks = ({ todolist }: Props) => {
+  const { data, isLoading } = useGetTasksQuery(todolist.id)
+
+  let tasksForTodolist = data?.items
+
+  if (todolist.filter === "active") {
+    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.New)
+  }
+
+  if (todolist.filter === "completed") {
+    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.Completed)
+  }
+
+  if (isLoading) {
+    return <TasksSkeleton />
+  }
+
+  return (
+    <>
+      {tasksForTodolist?.length === 0 ? (
+        <p>Тасок нет</p>
+      ) : (
+        <List>
+          {tasksForTodolist?.map((task) => {
+            return <Task key={task.id} task={task} todolist={todolist} />
+          })}
+        </List>
+      )}
+    </>
+  )
+}
