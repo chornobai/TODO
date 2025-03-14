08125af1bbbc43c0b23e72053e251b4c1ef3cfaa
veronikachernobai <bombeze13@gmail.com>
2025-03-14 19:45:29 +0300

    add

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,18 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { handleError } from "common/utils"
+import {handleError} from "common/utils";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   baseQuery: async (args, api, extraOptions) => {
-    const result = await fetchBaseQuery({
-      baseUrl: "https://social-network.samuraijs.com/api/1.1/",
+    const baseQuery = fetchBaseQuery({
+      baseUrl: process.env.REACT_APP_BASE_URL || "https://social-network.samuraijs.com/api/1.1/",
       prepareHeaders: (headers) => {
-        headers.set("API-KEY", `dc51b6a7-f90f-4230-9d2c-4a6c12ce1b73`)
-        headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
-        return headers
-      },
-    })(args, api, extraOptions)
+        headers.set("API-KEY", process.env.REACT_APP_API_KEY || "dc51b6a7-f90f-4230-9d2c-4a6c12ce1b73")
 
+        const token = localStorage.getItem("sn-token")
+        if (token) {
+          headers.set("Authorization", `Bearer ${token}`)
+        }
+      },
+    })
+
+    const result = await baseQuery(args, api, extraOptions)
     handleError(api, result)
 
     return result
