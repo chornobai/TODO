d7e04e188a317a15f9435cc3cfb84a68dcd7d92b
veronikachernobai <bombeze13@gmail.com>
2025-03-14 17:53:49 +0300

    add

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -5,9 +5,9 @@ export const baseApi = createApi({
   reducerPath: "todolistsApi",
   baseQuery: async (args, api, extraOptions) => {
     const result = await fetchBaseQuery({
-      baseUrl: process.env.REACT_APP_BASE_URL,
+      baseUrl: "https://social-network.samuraijs.com/api/1.1/",
       prepareHeaders: (headers) => {
-        headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
+        headers.set("API-KEY", `dc51b6a7-f90f-4230-9d2c-4a6c12ce1b73`)
         headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
         return headers
       },
