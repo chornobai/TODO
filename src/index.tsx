00be4ba2923a2bf81c8d314c1e0de385178d74dc
veronikachernobai <bombeze13@gmail.com>
2025-03-14 10:59:56 +0300

    add

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { store } from "./app/store"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}  />
   </Provider>,
 )
 
